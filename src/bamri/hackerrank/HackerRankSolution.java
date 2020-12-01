fdd9db5065c5bacf6d9009cc511a9729b9cd4235
Bilal AMRI <iambilalamri@gmail.com>
2020-12-01 19:57:51 +0100

    update package name

---
+++ src/bamri/hackerrank/HackerRankSolution.java
@@ -0,0 +1,12 @@
+package bamri.hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HackerRankSolution {
+
+	public List<Integer> getDiagonale(List<List<Integer>> matrix) {
+		return matrix.get(0);
+	}
+
+}
