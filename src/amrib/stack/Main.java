d588e78f01c6678b15ef5e83129c8494f88082e9
Bilal AMRI <iambilalamri@gmail.com>
2020-12-03 22:47:44 +0100

    [structure-algo]- working with stack

--- src/amrib/stack/Main.java
+++ src/amrib/stack/Main.java
@@ -9,17 +9,22 @@ public class Main {
 		stack.push(12);
 		stack.push(10);
 		stack.push(16);
-		System.out.println(stack);
+		// System.out.println(stack);
 		var top = stack.pop();
-		System.out.println(top);
-		System.out.println(stack);
+		// System.out.println(top);
+		// System.out.println(stack);
 		top = stack.peek();
-		System.out.println(top);
-		System.out.println(stack);
+		// System.out.println(top);
+		// System.out.println(stack);
 		//-------------------------
 		String str = "abcd";
 		StringReverser reverser = new StringReverser();
-		System.out.println(reverser.reverse(str));
+		// System.out.println(reverser.reverse(str));
+		//--------------------------
+		String str1 = "(a+b)";
+		Expression exp = new Expression();
+		var result = exp.isBalanced(str1);
+		System.out.println(result);
 		
 	}
 
